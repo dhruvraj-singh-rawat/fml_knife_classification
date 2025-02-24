a0d0bed3b85fe8fbfba47d05e7567c338fd280b6
Dhruvraj Singh Rawat <dhruvraj.rawat@gmail.com>
2025-02-24 01:31:34 +0000

    Added: Model Code

---
+++ train.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+STUDENT_ID=1234567890 STUDENT_NAME="Dhruvraj Singh Rawat" python main.py \
+--model_mode tf_efficientnet_b0 \
+--dataset_location ../EEEM066_Knife_Classification_dataset \
+--train_datacsv dataset/train.csv \
+--test_datacsv dataset/test.csv \
+--saved_checkpoint_path Knife-Effb0 \
+--epochs 20 \
+--batch_size 32 \
+--n_classes 192 \
+--learning_rate 0.00005 \
+--resized_img_weight 224 \
+--resized_img_height 224 \
+--seed 0 \
+--brightness 0.2 \
+--contrast 0.2 \
+--saturation 0.2 \
+--hue 0.2 \
+--optim rmsprop \
+--lr-scheduler CosineAnnealingLR
+--random_rotation 10
